90dcdcfe756405f613c76b6ae6b90cde1ee69b63
ViktorGakis <92163319+ViktorGakis@users.noreply.github.com>
2023-10-10 19:26:19 +0000

    core init utils

--- nvim/lua/vikt/core/init.lua
+++ nvim/lua/vikt/core/init.lua
@@ -1,3 +1,23 @@
 require("vikt.core.keymaps")
 require("vikt.core.options")
 -- require("vikt.core.clipboard")
+--
+-- -- disable some default providers
+for _, provider in ipairs({ "node", "perl", "python3", "ruby" }) do
+    vim.g["loaded_" .. provider .. "_provider"] = 0
+end
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
+
+-------------------------------------- autocmds ------------------------------------------
+local autocmd = vim.api.nvim_create_autocmd
+
+-- dont list quickfix buffers
+autocmd("FileType", {
+    pattern = "qf",
+    callback = function()
+        vim.opt_local.buflisted = false
+    end,
+})
--- nvim/lua/vikt/core/keymaps.lua
+++ nvim/lua/vikt/core/keymaps.lua
@@ -5,6 +5,22 @@ local g = vim.g
 g.mapleader = " "
 g.maplocalleader = " "
 ---------------------
+---- Generic Utils
+map("n", "<C-d>", "<C-d>zz", { desc = "<C-d> with centering in the end" })
+map("n", "<C-u>", "<C-u>zz", { desc = "<C-u> with centering in the end" })
+map("n", "<leader>o", "o<ESC>")
+map("n", "<leader>O", "O<ESC>")
+map("n", "<C-c>", "<cmd> %y+ <CR>", { desc = "Copy whole file" })
+map("n", "<C-z>", "<nop>")
+
+-- Don't copy the replaced text after pasting in visual mode
+-- https://vim.fandom.com/wiki/Replace_a_word_with_yanked_text#Alternative_mapping_for_paste
+map("x", "p", 'p:let @+=@0<CR>:let @"=@0<CR>', { desc = "Dont copy replaced text", silent = true })
+
+-- map("n", "<leader>fm", format_func, { desc = "LSP formatting" })
+map("n", "<C-s>", "<leader>mp<cmd> w <CR>", { desc = "Format and Save" })
+-- delete single character without copying into register
+map("n", "x", '"_x')
 -- General Keymaps -------------------
 
 -- use jk to exit insert mode
@@ -31,47 +47,27 @@ map("n", "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { desc = "Move up"
 map("n", "<Up>", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { desc = "Move up", expr = true })
 map("n", "<Down>", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { desc = "Move down", expr = true })
 
--- map("n", "<leader>fm", format_func, { desc = "LSP formatting" })
-map("n", "<C-s>", "<leader>mp<cmd> w <CR>", { desc = "Format and Save" })
--- delete single character without copying into register
-map("n", "x", '"_x')
-
 -- increment/decrement numbers
 map("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 map("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
--- window management
-map("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-map("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-map("n", "<leader>we", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & eight
-map("n", "<leader>wx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+---- window management
+map("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
+map("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally" })
+map("n", "<leader>we", "<C-w>=", { desc = "Make splits equal size" })
+map("n", "<leader>wx", "<cmd>close<CR>", { desc = "Close current split" })
+map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+map("n", "<leader><tab>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
-map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-map("n", "<leader><tab>", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
-map("n", "<C-d>", "<C-d>zz", { desc = "<C-d> with centering in the end" })
-map("n", "<C-u>", "<C-u>zz", { desc = "<C-u> with centering in the end" })
-
-map("n", "<leader>o", "o<ESC>")
-
-map("n", "<leader>O", "O<ESC>")
-
-map("n", "<C-c>", "<cmd> %y+ <CR>", { desc = "Copy whole file" })
-map("n", "<C-z>", "<nop>")
-
--- NVIMTREE
--- -- toggle
+---- NVIMTREE
 map("n", "<C-n>", "<cmd> NvimTreeToggle <CR>", { desc = "Toggle nvimtree" })
-
--- focus
 map("n", "<leader>e", "<cmd> NvimTreeFocus <CR>", { desc = "Focus nvimtree" })
--- refres  fil  explore
 map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
 
--- TELESCOPE KEYMAPS
+---- TELESCOPE KEYMAPS
 map("n", "<leader>ff", "<cmd> Telescope find_files <CR>", { desc = "Find files" })
 map("n", "<leader>fa", "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", { desc = "Find all" })
 map("n", "<leader>fw", "<cmd> Telescope live_grep <CR>", { desc = "Live grep" })
@@ -80,18 +76,17 @@ map("n", "<leader>fh", "<cmd> Telescope help_tags <CR>", { desc = "Help page" })
 map("n", "<leader>fo", "<cmd> Telescope oldfiles <CR>", { desc = "Find oldfiles" })
 map("n", "<leader>fz", "<cmd> Telescope current_buffer_fuzzy_find <CR>", { desc = "Find {desc=in current buffer" })
 
--- git
+---- git
 map("n", "<leader>cm", "<cmd> Telescope git_commits <CR>", { desc = "Git commits" })
 map("n", "<leader>gt", "<cmd> Telescope git_status <CR>", { desc = "Git status" })
 
--- pick a hidden term
 map("n", "<leader>pt", "<cmd> Telescope terms <CR>", { desc = "Pick hidden term" })
 
--- theme switcher
 map("n", "<leader>th", "<cmd> Telescope themes <CR>", { desc = "Nvchad themes" })
 
 map("n", "<leader>ma", "<cmd> Telescope marks <CR>", { desc = "telescope bookmarks" })
 
+---- Sessions
 map("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
 map("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
 --
@@ -99,10 +94,6 @@ map("n", "<leader>b", "<cmd> enew <CR>", { desc = "New buffer" })
 map("n", "<leader>ch", "<cmd> NvCheatsheet <CR>", { desc = "Mapping cheatsheet" })
 
 map("n", "<leader>sm", "<cmd>MaximizerToggle<CR>", { desc = "Maximize/minimize a split" })
-
--- Don't copy the replaced text after pasting in visual mode
--- https://vim.fandom.com/wiki/Replace_a_word_with_yanked_text#Alternative_mapping_for_paste
-map("x", "p", 'p:let @+=@0<CR>:let @"=@0<CR>', { desc = "Dont copy replaced text", silent = true })
 
 -- TABUFLINE
 --
