9a15c2efb8ca1fcc8c034aff582a281f603ea2b6
Viktor Gakis <92163319+ViktorGakis@users.noreply.github.com>
2023-10-14 21:18:54 +0300

    Update setup_nvim.sh

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,15 +1,15 @@
 # If ~/.config/nvim exists, remove it
-[ -d ~/.config/nvim ] && rm -rf ~/.config/nvim
-[ -d ~/.local/share/nvim ] && rm -rf ~/.local/share/nvim
-[ -d ~/.config/vscode ] && rm -rf ~/.config/vscode
+[ -d ~/.config/nvim/ ] && rm -rf ~/.config/nvim/
+[ -d ~/.local/share/nvim/ ] && rm -rf ~/.local/share/nvim/
+[ -d ~/.config/vscode/ ] && rm -rf ~/.config/vscode/
 [ -d ~/.config/.git ] && rm -rf ~/.config/.git
 
 [ -d ~/.config_temp/ ] && rm -rf ~/.config_temp/
 # Clone the entire repo directly into ~/.config
-# git clone --depth 1 https://github.com/ViktorGakis/dotfiles.git ~/.config_temp
+git clone --depth 1 https://github.com/ViktorGakis/dotfiles.git ~/.config_temp/
 
 # Move all the content from the temp cloned directory to ~/.config
 mv ~/.config_temp/* ~/.config_temp/.[!.]* ~/.config/
 
 # Remove the temporary directory
-rm -rf ~/.config_temp
+rm -rf ~/.config_temp/
