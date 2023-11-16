2dd55fe65e1af8e77e163da53d6674113b6a68c1
ViktorGakis <victor.g.gakis@gmail.com>
2023-11-16 03:15:45 +0200

    treesitter hl upd, eslint added in lint

--- nvim/lua/vikt/plugins/lsp/none-ls.lua
+++ nvim/lua/vikt/plugins/lsp/none-ls.lua
@@ -15,11 +15,11 @@ return {
         mason_null_ls.setup({
             ensure_installed = {
                 "prettier", -- prettier formatter
-                "stylua",   -- lua formatter
-                "black",    -- python formatter
+                "stylua", -- lua formatter
+                "black", -- python formatter
                 "ruff",
                 "mypy",
-                "pylint",   -- python linter
+                "pylint", -- python linter
                 "eslint_d", -- js linter
                 -- "csharpier",
                 -- "semgrep", -- C# linter
@@ -27,7 +27,7 @@ return {
         })
         local code_actions = null_ls.builtins.code_actions
         -- for conciseness
-        local formatting = null_ls.builtins.formatting   -- to setup formatters
+        local formatting = null_ls.builtins.formatting -- to setup formatters
         local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
         -- to setup format on save
@@ -47,7 +47,7 @@ return {
                 code_actions.marksman,
                 formatting.prettier.with({
                     extra_filetypes = { "svelte" },
-                }),                -- js/ts formatter
+                }), -- js/ts formatter
                 formatting.stylua, -- lua formatter
                 formatting.isort,
                 formatting.black,
@@ -68,7 +68,12 @@ return {
                     extra_args = { "--ignore", "e501", "--select", "e126" },
                 }),
                 diagnostics.shellcheck,
-                diagnostics.eslint_d.with({                                                               -- js/ts linter
+                diagnostics.eslint.with({ -- js/ts linter
+                    condition = function(utils)
+                        return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
+                    end,
+                }),
+                diagnostics.eslint_d.with({ -- js/ts linter
                     condition = function(utils)
                         return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" }) -- only enable if root has .eslintrc.js or .eslintrc.cjs
                     end,
